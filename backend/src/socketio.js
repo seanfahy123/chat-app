956a8d96a878db4844423bf3af62ae3937a64be3
Sean Fahy <Seanfahy45@gmail.com>
2019-10-09 06:51:37 +0100

    added save to db functionality to user messages

--- backend/src/socketio.js
+++ backend/src/socketio.js
@@ -1,5 +1,6 @@
 const io = require("socket.io")();
 const mongoose = require("mongoose");
+const Message = require("./models/message");
 
 io.on("connection", socket => {
   console.log("CONNECTED");
@@ -8,9 +9,12 @@ io.on("connection", socket => {
     socket.join(room);
   });
 
-  socket.on("sendMessage", message => {
+  socket.on("sendMessage", async message => {
     console.log("SENDING FROM SERVER");
     message._id = new mongoose.Types.ObjectId();
+    console.log(message);
+    const messageToSave = new Message(message);
+    await messageToSave.save();
     io.to(message.room).emit("message", message);
   });
 });
