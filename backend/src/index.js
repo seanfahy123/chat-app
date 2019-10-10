610c94f295397ca4d0dbb449394046f70a6d9027
Sean Fahy <Seanfahy45@gmail.com>
2019-10-10 16:06:14 +0100

    M

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,9 +19,9 @@ app.use(express.static(path.join(__dirname + "/frontend/build/")));
 app.use(userRouter);
 app.use(messageRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+// });
 
 var io = require("socket.io")(http);
 
