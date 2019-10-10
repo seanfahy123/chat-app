76599fd6fcb59c5c05f30d0cee841e1d0f63bc57
Sean Fahy <Seanfahy45@gmail.com>
2019-10-10 13:51:10 +0100

    m

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ app.use(userRouter);
 app.use(messageRouter);
 
 app.get("*", (req, res) => {
-  res.sendfile(path.join(__dirname + "./frontend/build/index.html"));
+  res.sendFile(path.join(__dirname + "./frontend/build/index.html"));
 });
 
 // app.use(express.static(path.join(__dirname + "./../../frontend/build")));
