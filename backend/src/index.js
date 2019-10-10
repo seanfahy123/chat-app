749d4aa9448a9d90d484492458601f2a3a36b6a2
Sean Fahy <Seanfahy45@gmail.com>
2019-10-10 16:43:46 +0100

    M

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,48 @@
-const express = require("express");
+const mongoose = require("mongoose");
+const Message = require("./models/message");
+const users = require("./utils/users");
 
-const app = express();
+// io.on("connection", socket => {
+//   socket.on("join", ({ username, room }) => {
+//     users.addUser(username, room, socket.id);
+//     socket.join(room);
+//     io.to(room).emit("presentUsers", users.getUsersInRoom(room));
+//   });
 
-var http = require("http").Server(app);
+//   socket.on("disconnect", () => {
+//     const disconnectingUser = users.removeUser(socket.id);
+//     if (disconnectingUser) {
+//       io.to(disconnectingUser.room).emit(
+//         "presentUsers",
+//         users.getUsersInRoom(disconnectingUser.room)
+//       );
+//     }
+//   });
 
-//var io = require("socket.io")(http);
+//   socket.on("sendMessage", async message => {
+//     message._id = new mongoose.Types.ObjectId();
+//     const messageToSave = new Message(message);
+//     await messageToSave.save();
+//     io.to(message.room).emit("message", messageToSave);
+//   });
+// });
 
-// vars past
+// const socketioport = process.env.SOCKETIO_PORT;
+
+// io.listen(socketioport);
+// console.log(`SocketIO server is up on ${socketioport}`);
+
+require("./db/mongoose");
+
+// const userRouter = require("./routers/user");
+// const messageRouter = require("./routers/message");
+
+// const path = require("path");
+// require("./socketio");
+// EHRENENE
+const app = require("express")();
+
+var io = require("socket.io")(app);
 
 const userRouter = require("./routers/user");
 const messageRouter = require("./routers/message");
@@ -15,17 +51,9 @@ const path = require("path");
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname + "/frontend/build/")));
 app.use(userRouter);
 app.use(messageRouter);
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
-// });
-
-var io = require("socket.io")(http);
-
-// app stuff done
+app.use(express.static(path.join(__dirname + "./../../frontend/build")));
 
 io.on("connection", socket => {
   socket.on("join", ({ username, room }) => {
@@ -52,9 +80,21 @@ io.on("connection", socket => {
   });
 });
 
-http.listen(process.env.PORT, () => {
-  console.log("list;ening now");
-});
+app.listen(process.env.PORT);
+
+// EHUSNFE
+// app.use(express.json());
+
+// app.use(userRouter);
+// app.use(messageRouter);
+// app.use(express.static(path.join(__dirname + "./../../frontend/build")));
+
+// const app = require("./app");
+// const expressport = process.env.EXPRESS_PORT;
+
+// app.listen(expressport, () => {
+//   console.log(`Express server is up on port ${expressport}`);
+// });
 
 // const app = require("./app");
 
