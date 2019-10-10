af97a90a1da0906258d9c702726eb26d6bc573d7
Sean Fahy <Seanfahy45@gmail.com>
2019-10-10 13:53:22 +0100

    m

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,12 +12,12 @@ require("./socketio");
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname + "./frontend/build/")));
+app.use(express.static(path.join(__dirname + "/frontend/build/")));
 app.use(userRouter);
 app.use(messageRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "./frontend/build/index.html"));
+  res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
 });
 
 // app.use(express.static(path.join(__dirname + "./../../frontend/build")));
