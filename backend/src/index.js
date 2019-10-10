52675d88fc18b7f55217a808bb797755e1d5539a
Sean Fahy <Seanfahy45@gmail.com>
2019-10-10 15:41:14 +0100

    M

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,35 @@
-//require("dotenv").config();
-
 const app = require("./app");
 
-app.listen(process.env.PORT || process.env.EXPRESS_PORT, "0.0.0.0");
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+io.on("connection", socket => {
+  socket.on("join", ({ username, room }) => {
+    users.addUser(username, room, socket.id);
+    socket.join(room);
+    io.to(room).emit("presentUsers", users.getUsersInRoom(room));
+  });
+
+  socket.on("disconnect", () => {
+    const disconnectingUser = users.removeUser(socket.id);
+    if (disconnectingUser) {
+      io.to(disconnectingUser.room).emit(
+        "presentUsers",
+        users.getUsersInRoom(disconnectingUser.room)
+      );
+    }
+  });
+
+  socket.on("sendMessage", async message => {
+    message._id = new mongoose.Types.ObjectId();
+    const messageToSave = new Message(message);
+    await messageToSave.save();
+    io.to(message.room).emit("message", messageToSave);
+  });
+});
+
+//require("dotenv").config();
+
+//const app = require("./app");
+
+http.listen(process.env.PORT || process.env.EXPRESS_PORT, "0.0.0.0");
