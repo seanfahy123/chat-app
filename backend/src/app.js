b7d625dc93763aeee53218ceea9f9b8a8f8e845f
Sean Fahy <Seanfahy45@gmail.com>
2019-10-10 15:47:54 +0100

    M

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,25 +1,23 @@
-require("./db/mongoose");
+// require("./db/mongoose");
 
-const express = require("express");
-const userRouter = require("./routers/user");
-const messageRouter = require("./routers/message");
+// const express = require("express");
+// const userRouter = require("./routers/user");
+// const messageRouter = require("./routers/message");
 
-const path = require("path");
+// const path = require("path");
 
-const app = express();
+// const app = express();
 
-require("./socketio");
+// app.use(express.json());
 
-app.use(express.json());
+// app.use(express.static(path.join(__dirname + "/frontend/build/")));
+// app.use(userRouter);
+// app.use(messageRouter);
 
-app.use(express.static(path.join(__dirname + "/frontend/build/")));
-app.use(userRouter);
-app.use(messageRouter);
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+// });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
-});
+// // app.use(express.static(path.join(__dirname + "./../../frontend/build")));
 
-// app.use(express.static(path.join(__dirname + "./../../frontend/build")));
-
-module.exports = app;
+// module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,31 @@
-const app = require("./app");
+const express = require("express");
+
+const app = express();
 
 var http = require("http").Server(app);
+
 var io = require("socket.io")(http);
+
+// vars past
+
+const userRouter = require("./routers/user");
+const messageRouter = require("./routers/message");
+
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname + "/frontend/build/")));
+app.use(userRouter);
+app.use(messageRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+});
+
+// app stuff done
 
 io.on("connection", socket => {
   socket.on("join", ({ username, room }) => {
@@ -28,8 +52,42 @@ io.on("connection", socket => {
   });
 });
 
-//require("dotenv").config();
+http.listen(process.env.PORT, () => {
+  console.log("listening now");
+});
 
-//const app = require("./app");
+// const app = require("./app");
 
-http.listen(process.env.PORT || process.env.EXPRESS_PORT, "0.0.0.0");
+// var http = require("http").Server(app);
+// var io = require("socket.io")(http);
+
+// io.on("connection", socket => {
+//   socket.on("join", ({ username, room }) => {
+//     users.addUser(username, room, socket.id);
+//     socket.join(room);
+//     io.to(room).emit("presentUsers", users.getUsersInRoom(room));
+//   });
+
+//   socket.on("disconnect", () => {
+//     const disconnectingUser = users.removeUser(socket.id);
+//     if (disconnectingUser) {
+//       io.to(disconnectingUser.room).emit(
+//         "presentUsers",
+//         users.getUsersInRoom(disconnectingUser.room)
+//       );
+//     }
+//   });
+
+//   socket.on("sendMessage", async message => {
+//     message._id = new mongoose.Types.ObjectId();
+//     const messageToSave = new Message(message);
+//     await messageToSave.save();
+//     io.to(message.room).emit("message", messageToSave);
+//   });
+// });
+
+// //require("dotenv").config();
+
+// //const app = require("./app");
+
+// http.listen(process.env.PORT || process.env.EXPRESS_PORT, "0.0.0.0");
